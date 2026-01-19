f1a2f30d0e2486509313287d92fbb2357c01a2e5
Sohel Mallik <malliksohel582@gmail.com>
2026-01-19 23:07:58 +0530

    Continue Statement Commited

---
+++ _3Loop.java/_11Continue_Statement.java
@@ -0,0 +1,10 @@
+public class _11Continue_Statement {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i % 2 == 0) {
+                continue; // Skip even numbers
+            }
+            System.out.println(i); // Print only odd numbers
+        }
+    }
+}
