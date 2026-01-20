60a2323eadc5e23809d4e9fe02fd8b00cdae7d2b
Sohel Mallik <malliksohel582@gmail.com>
2026-01-20 22:32:26 +0530

    Break Statement Commited

---
+++ _3Loop.java/_12break.java
@@ -0,0 +1,10 @@
+public class _12break {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i == 5) {
+                break; // Exit the loop when i is 5
+            }
+            System.out.println(i); // Print numbers from 1 to 4
+        }
+    }
+}
