d9cd57f3bd2817488d7419e1976faf821b7777c4
Sohel Mallik <malliksohel582@gmail.com>
2026-01-29 00:02:25 +0530

    Infinite Loop commited

---
+++ _3Loop.java/_13Infunite_loop.java
@@ -0,0 +1,36 @@
+public class _13Infunite_loop {
+    public static void main(String[] args) {
+        // Infinite loop using for loop
+        for (;;) {
+            System.out.println("This loop will run forever.");
+        }
+
+    }
+}
+
+/*Common Ways to Create Infinite Loops
+1️⃣ Infinite while Loop
+while (true) {
+    System.out.println("Infinite loop");
+}
+
+
+2️⃣ Infinite for Loop
+for (;;) {
+    System.out.println("Infinite loop");
+}
+
+
+3️⃣ Infinite do-while Loop
+do {
+    System.out.println("Infinite loop");
+} while (true);
+
+
+4️⃣ Condition Never Changes
+int i = 1;
+while (i > 0) {   // i never changes
+    System.out.println(i);
+}
+
+*/
