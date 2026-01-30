2b473c702b05a9807f706fbff6e60568ddd77fa3
Sohel Mallik <malliksohel582@gmail.com>
2026-01-30 20:27:48 +0530

    Commit

--- _3Loop.java/_9Prime.java
+++ _3Loop.java/_9Prime.java
@@ -1,27 +1,29 @@
 import java.util.Scanner;
+
 public class _9Prime {
     public static void main(String[] args) {
-       Scanner sc=new Scanner(System.in);
-       System.out.print("Enter a number to check if it's prime:");
-       int n=sc.nextInt();
-       sc.close();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number to check if it's prime: ");
+        int n = sc.nextInt();
+        sc.close();
+
+        // 0 and 1 are neither prime nor composite
+        if (n == 0 || n == 1) {
+            System.out.println(n + " is neither prime nor composite.");
+            return;
+        }
 
         boolean isPrime = true;
-        for (int i=2; i <= Math.sqrt(n); i++) {
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
             if (n % i == 0) {
                 isPrime = false;
                 break;
             }
         }
 
-        if(n==1 || n==0){
-<<<<<<< HEAD
-            System.out.println(n + ":  is neither prime nor composite.");
-=======
-            System.out.println("1 is neither prime nor composite.");
->>>>>>> b9b18dd9bee829d245a1ca466d8da2e7c03dbd77
-        }
-        else if (isPrime==true) {
+        if (isPrime) {
             System.out.println(n + " is a prime number.");
         } else {
             System.out.println(n + " is a composite number.");
