0777a26f0cac0b10c05b632e15c71546b6a4e843
Sohel Mallik <malliksohel582@gmail.com>
2026-01-30 20:21:43 +0530

    Commited

---
+++ _3Loop.java/_14While_loop.java
@@ -0,0 +1,14 @@
+public class _14While_loop {
+    public static void main(String[] args) {
+        int i=1;
+        // while(i<=8){
+        //     System.out.println(i);
+        //     i++;
+        // }
+
+        for(;i<=8;){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
