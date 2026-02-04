ece9e7a086e9b66ba0ff741fc76fa6db7ca4db52
Sohel Mallik <malliksohel582@gmail.com>
2026-02-04 22:22:18 +0530

    commited

---
+++ LOGIC.java/_1RangeExample.java
@@ -0,0 +1,25 @@
+
+public class _1RangeExample {
+    public static void main(String[] args) {
+        System.out.println("int range: " + Integer.MIN_VALUE + " to " + Integer.MAX_VALUE+"\n");
+        System.out.println("byte range: " + Byte.MIN_VALUE + " to " + Byte.MAX_VALUE+"\n");
+        System.out.println("short range: " + Short.MIN_VALUE + " to " + Short.MAX_VALUE+"\n");
+        System.out.println("long range: " + Long.MIN_VALUE + " to " + Long.MAX_VALUE+"\n");
+        System.out.println("float range: " + Float.MIN_VALUE + " to " + Float.MAX_VALUE+"\n");
+        System.out.println("double range: " + Double.MIN_VALUE + " to " + Double.MAX_VALUE+"\n");
+    }
+}
+
+
+
+// public   class _1RnageMaxFind {
+//     public static void main(String[] args) {
+//         int x = Integer.MAX_VALUE;
+//         int y = Integer.MIN_VALUE;
+//         System.out.println("Integer.MAX_VALUE: " + x);
+//         System.out.println("Integer.MIN_VALUE: " + y);
+
+
+        
+//     }
+// }
