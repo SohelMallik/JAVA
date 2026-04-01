f03444325d1225780902310438412673416c8b84
Sohel Mallik <malliksohel582@gmail.com>
2026-04-02 00:38:05 +0530

    Commited

--- _5Method.java/_11StaticVsInstance.java
+++ _5Method.java/_11StaticVsInstance.java
@@ -1,17 +1,18 @@
 public class _11StaticVsInstance {
   
-    public void show() {
-        System.out.println("instance");
-    }
+    // public void show() { 
+    //     System.out.println("instance");
+    // }
 
-    public static void show(int a) {
+    public static void show(int a) {// Sudhu parameter thakalea
         System.out.println("static");
     }
 
     public static void main(String[] args) {
         _11StaticVsInstance obj = new _11StaticVsInstance();
-        obj.show();
-        obj.show(10);
+        // obj.show();
+        // obj.show(10); // Error: non-static method show(int) cannot be referenced from a static context
+        show(6); // Calls the static method    
     }
 }
 
