f5b6fab082b931d78c8193213847f62a9f4bbe4b
Sohel Mallik <malliksohel582@gmail.com>
2026-04-04 22:06:51 +0530

    commited

---
+++ _5Method.java/_16InstanceMethod.java
@@ -0,0 +1,11 @@
+public class _16InstanceMethod {
+    void show(){
+        System.out.println("Instance Method");
+    }
+
+    public static void main (String [] args){
+        _16InstanceMethod d= new _16InstanceMethod();
+        d.show();
+    }
+    
+}
