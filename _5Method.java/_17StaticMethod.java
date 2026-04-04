1f3c9819f4ac09288bc87b9f0d74faff0ab4f047
Sohel Mallik <malliksohel582@gmail.com>
2026-04-04 22:08:35 +0530

    _5Method.java/_17StaticMethod.java

---
+++ _5Method.java/_17StaticMethod.java
@@ -0,0 +1,14 @@
+public class _17StaticMethod {
+   
+   static void show(){
+        System.out.println("Instance Method");
+    }
+
+    public static void main (String [] args){
+        
+        _17StaticMethod.show();
+        show();
+    }
+    
+}
+
