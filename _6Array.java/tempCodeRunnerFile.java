7b8935e49a5551854183bc34ad7dc708f6b038ec
Sohel Mallik <malliksohel582@gmail.com>
2026-06-04 19:50:30 +0530

    Commited

--- _6Array.java/tempCodeRunnerFile.java
+++
@@ -1,9 +0,0 @@
-Scanner sc=new Scanner(System.in);
-        // System.out.print("Enter size of array: ");
-        // int n=sc.NextInt();
-
-        // int [] arr= new arr[n];
-
-        // for(int i=0; i<arr.length; i++){
-        //     arr[i]=sc.nextInt();
-        // }
